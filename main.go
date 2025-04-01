579418e86a10224ddb24e3e2191e07cc5bd25842
Tripp Hopkins <tripphopkins13245@gmail.com>
2025-03-31 22:05:21 -0400

    Added to main func

--- main.go
+++ main.go
@@ -1,17 +1,34 @@
 package main
 
+/*
+Making a new type "Config" and making it a struct
+the struct holds 3 values, all of which are drawn from a json file
+*/
+
 type Config struct {
 	IPAdress     string `json:"IPAdress"`
 	SerialNumber string `json:"SerialNumber"`
-	AccessCode   string `json:"AccessCode`
+	AccessCode   string `json:"AccessCode"`
 }
 
-func main() {
-	for i := 0; i < len(config.json); i++ {
+/*
 
-	}
+*/
+func main(){
 
+err := printer.Connect()
+if err != nil {
+    panic(err)
 }
+
+pool := bambulabs_api.NewPrinterPool()
+
+for i := 0; i < len(Config[]); i++{
+	pool.AddPrinter(Config)
+}
+}
+
+
 
 /*"IPAddress": " ",
 "SerialNumber": " ",
