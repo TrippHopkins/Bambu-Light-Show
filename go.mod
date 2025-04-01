9b0e4e6d4bcaeb3dc2f849621bd4af2ce510dc89
TrippHopkins <tripphopkins13245@gmail.com>
2025-04-01 18:25:20 -0400

    Did troubleshooting

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/eclipse/paho.mqtt.golang v1.5.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/secsy/goftp v0.0.0-20200609142545-aa2de14babf4 // indirect
-	github.com/torbenconto/bambulabs_api v0.1.0 // indirect
+	github.com/torbenconto/bambulabs_api v0.1.0-rc1 // indirect
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,70 +1,135 @@
+// package main
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"os"
+
+// 	"github.com/torbenconto/bambulabs_api"
+// 	"github.com/torbenconto/bambulabs_api/light"
+// )
+
+// /*
+// Making a new type "Config" and making it a struct
+// the struct holds 3 values, all of which are drawn from a json file
+// */
+
+// type Config []struct {
+// 	Host       string `json:"host"`
+// 	AccessCode string `json:"access_code"`
+// 	Serial     string `json:"serial_number"`
+// }
+
+// /*
+//  */
+// func main() {
+
+// 	// Open the JSON file
+// 	file, err := os.Open("config.json")
+// 	// Check for errors
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	var configs Config
+
+// 	// Create a new decoder and decode the JSON into the Config struct
+
+// 	err = json.NewDecoder(file).Decode(&configs)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	// Create a new pool for all printers
+// 	pool := bambulabs_api.NewPrinterPool()
+
+// 	//new Variable with type Config
+
+// 	//Iterate over json file and add all printers to the pool
+// 	//all printers have a IP Address, Serial Number and Access Code
+// 	for _, printer := range configs {
+// 		pool.AddPrinter(&bambulabs_api.PrinterConfig{
+// 			Host:         printer.Host,
+// 			AccessCode:   printer.AccessCode,
+// 			SerialNumber: printer.Serial,
+// 		})
+// 	}
+
+// 	//
+// 	pool.ConnectAll()
+
+// 	//Telling all the printers in the pool to turn on the chamber light
+// 	//The Fuction "ExecuteAll" requires a return of a printer and reference to the light
+// 	err = pool.ExecuteAll(func(p *bambulabs_api.Printer) error {
+// 		return p.LightOn(light.ChamberLight)
+// 	})
+// 	if err != nil {
+// 		fmt.Printf("Error during ExecuteAll: %v\n", err)
+// 	}
+// 	//disconnect from all printers in the pool
+// 	pool.DisconnectAll()
+// }
+
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 
 	"github.com/torbenconto/bambulabs_api"
 	"github.com/torbenconto/bambulabs_api/light"
 )
 
-/*
-Making a new type "Config" and making it a struct
-the struct holds 3 values, all of which are drawn from a json file
-*/
-
-type Config struct {
-	IPAddress    string `json:"IPAddress"`
-	SerialNumber string `json:"SerialNumber"`
-	AccessCode   string `json:"AccessCode"`
+type Config []struct {
+	Host       string `json:"host"`
+	AccessCode string `json:"access_code"`
+	Serial     string `json:"serial_number"`
 }
 
-/*
- */
 func main() {
-
-	// Open the JSON file
+	// read json file
 	file, err := os.Open("config.json")
-
-	// Check for errors
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
 
-	// Create a new pool for all printers
-	pool := bambulabs_api.NewPrinterPool()
+	// read files
+	var configs Config
 
-	//new Variable with type Config
-	PrinterConfigs := []Config{}
-
-	//Iterate over json file and add all printers to the pool
-	//all printers have a IP Address, Serial Number and Access Code
-	for _, Config := range PrinterConfigs {
-		pool.AddPrinter(&bambulabs_api.PrinterConfig{
-			Host:         Config.IPAddress,
-			SerialNumber: Config.SerialNumber,
-			AccessCode:   Config.AccessCode,
-		})
+	err = json.NewDecoder(file).Decode(&configs)
+	if err != nil {
+		panic(err)
 	}
 
-	//
+	fmt.Println(configs)
+
+	pool := bambulabs_api.NewPrinterPool()
+
+	for _, printer := range configs {
+		pool.AddPrinter(
+			&bambulabs_api.PrinterConfig{
+				Host:         printer.Host,
+				AccessCode:   printer.AccessCode,
+				SerialNumber: printer.Serial,
+			},
+		)
+	}
+
 	err = pool.ConnectAll()
 	if err != nil {
 		panic(err)
 	}
 
-	//Telling all the printers in the pool to turn on the chamber light
-	//The Fuction "ExecuteAll" requires a return of a printer and reference to the light
-	err = pool.ExecuteAll(func(printer *bambulabs_api.Printer) error {
-		return printer.LightOn(light.ChamberLight)
+	// printers := pool.GetPrinters()
+
+	// for _, printer := range printers {
+	//     time.Sleep(250 * time.Millisecond)
+	//     printer.LightOn(light.ChamberLight)
+	// }
+
+	pool.ExecuteAll(func(p *bambulabs_api.Printer) error {
+		return p.LightOn(light.ChamberLight)
 	})
 
-	if err != nil {
-		panic(err)
-	}
-	//disconnect from all printers in the pool
-	err = pool.DisconnectAll()
-	if err != nil {
-		panic(err)
-	}
 }
