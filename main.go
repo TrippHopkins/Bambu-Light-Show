9428ed8af7a7b9759dc92704f24f2a452ae16694
TrippHopkins <tripphopkins13245@gmail.com>
2025-03-31 21:31:47 -0400

    Added test json and started main.go operations

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+type Config struct {
+	IPAdress     string `json:"IPAdress"`
+	SerialNumber string `json:"SerialNumber"`
+	AccessCode   string `json:"AccessCode`
+}
+
+func main() {
+	for i := 0; i < len(config.json); i++ {
+
+	}
+
+}
+
+/*"IPAddress": " ",
+"SerialNumber": " ",
+"AccessCode": " "
+}*/
